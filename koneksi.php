7996ba7fc811b03495ee560c8ec052f95dfe7e8c
Muhammad Rizaldi <mrizaldi1998@gmail.com>
2020-11-11 11:25:07 +0800

    mntab

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_connect("rutik.id", "u1005574_rizaldi", "5yqYxumAGvshJRF", "u1005574_uts_implementasi");
 
-if (!$conn) {
-    echo "koneksi gagal";
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal : " . mysqli_connect_error();
 }
